1c2da33e7f6d1cbafa0a7d7e6a7a429de733b361
Egor Chernyshov <jarogor@ya.ru>
2024-09-22 14:14:09 +0300

    E:\music\progressive metal

---
+++ VkMusicToTelegram/PostHandlers/Handlers/EProgressiveMetal.cs
@@ -0,0 +1,14 @@
+﻿using VkMusicToTelegram.Dto;
+
+namespace VkMusicToTelegram.PostHandlers.Handlers;
+
+/// <summary>
+/// - первая строка это название `группа - альбом (год)`
+/// - название группы может выглядеть так `[club787866|MINUALA]`, поэтому его нужно проверять регуляркой и вырезать
+/// - последняя строка это теги
+/// </summary>
+public sealed class EProgressiveMetal : Handler1Base {
+    public override string Domain => "progressivemetal";
+    public override string Name => @"E:\music\progressive metal";
+    public override Record GetPreparedTitle(Post post) => base.PrepareTitle(post);
+}
