2cd9e307fc3f0a6b7defd9d70ae31970883b9ace
Egor Chernyshov <jarogor@ya.ru>
2024-09-22 14:17:28 +0300

    E:\music\post

--- VkMusicToTelegram/Constants.cs
+++ VkMusicToTelegram/Constants.cs
@@ -13,6 +13,7 @@ public static class Constants {
         ("asylumforesters_vk", "Убежище Лесников", new AsylumForesters(), new TopCount(300, 50)),
         ("black_metal_promotion", "Black Metal Promotion", new BlackMetalPromotion(), new TopCount(25, 10)),
         ("e_black_metal", @"E:\music\black metal", new EBlackMetal(), new TopCount(25, 10)),
+        ("post_music", @"E:\music\post", new EPost(), new TopCount(35, 15)),
         ("progressivemetal", @"E:\music\progressive metal", new EProgressiveMetal(), new TopCount(25, 10)),
         ("ru_black_metal", "Русский блэк-метал", new RuBlackMetal(), new TopCount(100, 30)),
     ];
